4e84e343c76a9700569770807c9adf442b00a3aa
kp-marczynski <kp.marczynski@gmail.com>
2019-10-30 18:25:20 +0100

    improvements

---
+++ lab1/remove-logs.sh
@@ -0,0 +1,18 @@
+rm -r z0/p1/log
+rm -r z0/p2/log
+
+rm -r z1/p1/log
+rm -r z1/p2/log
+rm -r z1/p3/log
+
+rm -r z2/p1/log
+rm -r z2/p2/log
+rm -r z2/p3/log
+
+rm -r z3/p1/log
+rm -r z3/p2/log
+rm -r z3/p3/log
+
+rm -r z4/p1/log
+rm -r z4/p2/log
+rm -r z4/p3/log
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 ##Wykonanie testów
     docker-compose up --build -d
     docker exec -it client bash
-    curl -x z<nr_zadania>_x<nr_maszyny>:3128 pics4learning.com
+    curl -x z<nr_zadania>_x<nr_maszyny>:3128 http://www.deepsloweasy.com/
